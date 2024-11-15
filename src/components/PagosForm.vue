dfec6321a5791ea9471c7e3811d4f7479a1bc514
Niicor <niicorivero@gmail.com>
2024-11-15 17:33:20 -0300

    h5 datos de pago

--- src/components/PagosForm.vue
+++ src/components/PagosForm.vue
@@ -3,7 +3,7 @@
     <div class="card">
         <div class="card-body">
     <div class="container">
-      <h3>Datos de Pago</h3>
+      <h5>Datos de Pago</h5>
       <div class="row">
         <div class="col-md-12">
           <label for="cardNumber">Número de Tarjeta:</label>
